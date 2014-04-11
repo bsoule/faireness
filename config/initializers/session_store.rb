bfa89bb28da8dc711d4e09f41d5be0bac9b8825c
Bethany Marie Soule <bsoule@gmail.com>
2014-04-11 10:11:36 -0700

    Faireness 2.0

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Faireness::Application.config.session_store :cookie_store, key: '_faireness_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Faireness::Application.config.session_store :active_record_store

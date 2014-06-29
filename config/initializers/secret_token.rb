eb03c92e0dd8e2d718cf213a21d3f08a148d56be
Bethany Marie Soule <bsoule@gmail.com>
2014-06-29 11:08:23 -0700

    blow away the rails app :q

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Faireness::Application.config.secret_token = 'b89ba83f1616d81641d0079504553ee47d7ba48fd0ca3442917097c9e6f83d0c92d78679ab4f5c497585ff926dd7a3d3f8cb3332addca2becd6919ea154eea35'
